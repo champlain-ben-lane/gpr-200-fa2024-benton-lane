5e335df8fb2f567c7892a1f4978d973476357324
champlain-ben-lane <123412264+champlain-ben-lane@users.noreply.github.com>
2024-09-13 11:13:58 -0400

    Adding new files
    - Added files for core library
    - Added vertex and shader files

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -15,20 +15,25 @@ const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
 	"layout (location = 1) in vec4 aColor;\n"
 	"out vec4 Color;\n"
+	"uniform float uTime;\n"
 	"void main()\n"
 	"{\n"
 	"   Color = aColor;\n"
-	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"   vec3 pos = aPos;\n"
+	"   pos.y += sin(uTime * 5.0 + pos.x) / 4.0;\n" // This is the line that makes the triangle bounce up and down
+	"   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
 	"}\0";
 
 //Fragment shader source
 const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec4 Color;\n"
-"void main()\n"
-"{\n"
-"   FragColor = Color;\n"
-"}\n\0";
+	"out vec4 FragColor;\n"
+	"in vec4 Color;\n"
+	"uniform float uTime = 1.0f;\n"
+	"uniform vec4 uColor = vec4(1.0);\n"
+	"void main()\n"
+	"{\n"
+	"   FragColor = Color * (sin(uTime) * 5.0 * 0.5 + 0.5);\n" // This is the line that makes the colors go weeeee
+	"}\n\0";
 
 int main() {
 	printf("Initializing...");
@@ -96,9 +101,10 @@ int main() {
 
 	//Triangle(s)
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, //left
-		 0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f, 1.0f, //right
-		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f	//top
+	//    X      Y     Z     R     G     B     A
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+		 0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f, 1.0f,
+		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
 	};
 
 	// Time to put it all together
@@ -126,14 +132,20 @@ int main() {
 	while (!glfwWindowShouldClose(window)) {
 		
 		glfwPollEvents();
+
+		float time = (float)glfwGetTime();
 		
 		//Clear framebuffer
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClearColor(0.3f, 0.4f, 0.9f, 1.0f); // This is the background color assignment, lovely shade of blue at the moment
 		glClear(GL_COLOR_BUFFER_BIT);
 		
 		//Drawing happens here!
 		glUseProgram(shaderProgram);
+		int timeLoc = glGetUniformLocation(shaderProgram, "uTime");
+		glUniform1f(timeLoc, time);
 		glBindVertexArray(VAO);
+		
+		//Draw call
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);

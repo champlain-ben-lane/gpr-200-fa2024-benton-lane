93ad7fc8954bcb3980ad66558a950b4b80b5d8fe
Benton-Lane <benton.lane@mymail.champlain.edu>
2024-09-15 21:20:20 -0400

    Split Shader class
    - Ripped shader class code and dumped into .cpp file
    - Removed deprecated lines from the tutorials

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -11,31 +11,6 @@ using namespace std;
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
-/*Vertex shader source
-const char* vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"layout (location = 1) in vec4 aColor;\n"
-	"out vec4 Color;\n"
-	"uniform float uTime;\n"
-	"void main()\n"
-	"{\n"
-	"   Color = aColor;\n"
-	"   vec3 pos = aPos;\n"
-	"   pos.y += sin(uTime * 5.0 + pos.x) / 4.0;\n" // This is the line that makes the triangle bounce up and down
-	"   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
-	"}\0";
-
-//Fragment shader source
-const char* fragmentShaderSource = "#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"in vec4 Color;\n"
-	"uniform float uTime = 1.0f;\n"
-	"uniform vec4 uColor = vec4(1.0);\n"
-	"void main()\n"
-	"{\n"
-	"   FragColor = Color * (sin(uTime) * 5.0 * 0.5 + 0.5);\n" // This is the line that makes the colors go weeeee
-	"}\n\0";*/
-
 int main() {
 	printf("Initializing...");
 	if (!glfwInit()) {
@@ -52,54 +27,6 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
-	//Initialization goes here...? Yeah, here is good.
-
-	/*Vertex shader creation
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	//Vertex shader error check
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
-	}
-
-	//Fragment shader creation
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	//Fragment shader error check
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
-	}
-
-	//Time to make a shader program (yo dawg, we heard you like programs, so we made a program inside your program)
-	unsigned int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	//Shader program error check
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	}
-
-	//Delete the shaders, don't need 'em anymore
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-	*/
 
 	Shader ourShader("assets/vertexShader.vert", "assets/fragmentShader.frag");
 

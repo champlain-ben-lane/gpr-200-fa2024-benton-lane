9be8e66634fef74f55482019631bc3567807a3e9
Benton-Lane <benton.lane@mymail.champlain.edu>
2024-09-25 20:53:04 -0400

    Background & Foreground
    - Updated former frag and vert files to new names and added new files to handle the character sprite
    - Created the necessary code blocks (vertices and indices) to display the separate "layers"

--- assignments/assignment_2/main.cpp
+++ assignments/assignment_2/main.cpp
@@ -30,36 +30,78 @@ int main() {
 		return 1;
 	}
 
-	Shader ourShader("assets/vertexShader.vert", "assets/fragmentShader.frag");
+	Shader backgroundShader("assets/shaders/backgroundShader.vert", "assets/shaders/backgroundShader.frag");
+	Shader characterShader("assets/shaders/characterShader.vert", "assets/shaders/characterShader.frag");
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
-	float vertices[] = {
+
+	//Info for background vertices and indices
+	float verticesBG[] = {
+		// positions          // colors           // texture coords
+		 1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   10.0f, 10.0f, // top right
+		 1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   10.0f, 0.0f, // bottom right
+		-1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+		-1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 10.0f  // top left 
+	};
+	unsigned int indicesBG[] = {  // note that we start from 0!
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
+
+	//Info for foreground vertices and indices
+	float verticesFG[] = {
 		// positions          // colors           // texture coords
 		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
 		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
 		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
 		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
 	};
-	unsigned int indices[] = {  // note that we start from 0!
+	unsigned int indicesFG[] = {  // note that we start from 0!
 		0, 1, 3,  // first Triangle
 		1, 2, 3   // second Triangle
 	};
 
 	// Time to put it all together
-	unsigned int VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-	glBindVertexArray(VAO);
+	unsigned int VAO_BG, VAO_FG, VBO_BG, EBO_BG, VBO_FG, EBO_FG;
+	glGenVertexArrays(1, &VAO_BG);
+	glGenVertexArrays(1, &VAO_FG);
+	glGenBuffers(1, &VBO_BG);
+	glGenBuffers(1, &EBO_BG);
+	glGenBuffers(1, &VBO_FG);
+	glGenBuffers(1, &EBO_FG);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO_BG);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_BG);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesBG), verticesBG, GL_STATIC_DRAW);
 	// Could also use glNamedBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW) instead of the previous two lines
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_BG);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesBG), indicesBG, GL_STATIC_DRAW);
+
+	// Position info
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Color info 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// Texture coordinate info
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO_FG);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_FG);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesFG), verticesFG, GL_STATIC_DRAW);
+	// Could also use glNamedBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW) instead of the previous two lines
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_FG);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesFG), indicesFG, GL_STATIC_DRAW);
 
 	// Position info
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -75,7 +117,7 @@ int main() {
 
 	// load and create a texture 
 	// -------------------------
-	unsigned int texture1, texture2;
+	unsigned int texture1, texture2, texture3;
 
 	// texture 1
 	// ---------
@@ -125,14 +167,45 @@ int main() {
 	}
 	stbi_image_free(data);
 
+	// texture 3
+	// ---------
+	glGenTextures(1, &texture3);
+	glBindTexture(GL_TEXTURE_2D, texture3);
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// load image, create texture and generate mipmaps
+	data = stbi_load("assets/textures/portal_cube.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
 	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
 	// -------------------------------------------------------------------------------------------
-	ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+	backgroundShader.use(); // don't forget to activate/use the shader before setting uniforms!
 	// either set it manually like so:
-	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+	//glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
 	// or set it via the texture class
-	ourShader.setInt("texture2", 1);
+	backgroundShader.setInt("texture1", 0);
+	backgroundShader.setInt("texture2", 1);
 
+	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+	// -------------------------------------------------------------------------------------------
+	characterShader.use(); // don't forget to activate/use the shader before setting uniforms!
+	// either set it manually like so:
+	//glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+	// or set it via the texture class
+	characterShader.setInt("texture3", 0);
 
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -154,10 +227,23 @@ int main() {
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
 		//Render the container
-		ourShader.use();
-		int timeLoc = glGetUniformLocation(ourShader.ID, "uTime");
+		backgroundShader.use();
+		int timeLoc = glGetUniformLocation(backgroundShader.ID, "uTime");
 		glUniform1f(timeLoc, time);
-		glBindVertexArray(VAO);
+		glBindVertexArray(VAO_BG);
+
+		//Draw call
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		// bind textures on corresponding texture units
+		glActiveTexture(GL_TEXTURE3);
+		glBindTexture(GL_TEXTURE_2D, texture3);
+
+		//Render the container
+		characterShader.use();
+		//int timeLoc = glGetUniformLocation(characterShader.ID, "uTime");
+		glUniform1f(timeLoc, time);
+		glBindVertexArray(VAO_FG);
 
 		//Draw call
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -165,9 +251,9 @@ int main() {
 		glfwSwapBuffers(window);
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	glDeleteVertexArrays(1, &VAO_BG);
+	glDeleteBuffers(1, &VBO_BG);
+	glDeleteBuffers(1, &EBO_BG);
 
 	glfwTerminate();
 

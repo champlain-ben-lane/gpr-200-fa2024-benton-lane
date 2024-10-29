f5e14424a78c10daeb5c392088ed83dbe2483cc0
Benton-Lane <benton.lane@mymail.champlain.edu>
2024-10-29 18:40:03 -0400

    Phirst Phong
    - Got a basic Phong lighting model added to main and the shader program from the basic lighting tutorial

--- assignments/assignment_5/main.cpp
+++ assignments/assignment_5/main.cpp
@@ -194,6 +194,7 @@ int main() {
 		lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
 		lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 		lightingShader.setVec3("lightPos", lightPos);
+		lightingShader.setVec3("viewPos", cameraPos);
 
 
 		// Passing ze projection matrix to ze shader
